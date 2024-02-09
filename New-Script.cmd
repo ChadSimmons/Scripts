64f6348c2e61fdd9c1a4b6995b66201b2486ff05
Chad Simmons <Chad.Simmons@hotmail.com>
2024-02-09 16:55:34 -0600

    misc

--- New-Script.cmd
+++ New-Script.cmd
@@ -4,7 +4,7 @@ set myCustomVariable=myCustomValue
 REM ####################################################################################################################
 If /I "%1"=="/h" goto:about
 If /I "%1"=="/help" goto:about
-goto:main
+goto:Initialize
 :about
 echo ###################################################################################################################
 echo .Synopsis
@@ -33,9 +33,34 @@ echo     ===== Additional References and Reading =====
 echo     - <link title>: https://domain.url
 echo ###################################################################################################################
 goto:eof
+:Initialize
 setlocal
 set ScriptPath=%~dp0
 set ScriptPath=%ScriptPath:~,-1%
+set LogFile=%ProgramData%\Logs\%~n0.Log
+If NOT EXIST %LogFile% (MkDir %ProgramData%\Logs & ECHO Timestamp,Status,Message > %LogFile%)
+goto:main
+
+:LogMessage
+:: .Synopsis - Write timestamped message to a file or the console
+:: .Parameter1 - [mandatory] Message to be logged (must be double-quoted if it contains spaces)
+:: .Parameter2 - [optional] Status (Info, Warn, Error)
+If [%1]==[] goto:eof
+If [%LogFile%]==[] ECHO [%DATE:~10,4%-%date:~4,2%-%date:~7,2% %TIME:~0,8%] %2	%1
+If NOT [%LogFile%]==[] ECHO %DATE:~10,4%-%date:~4,2%-%date:~7,2% %TIME:~0,8%,%2,%1 >> "%LogFile%"
+goto:eof
+
 :main
+Call:LogMessage "========== Starting script %~0" "INFO"
+
+
+
+ping 127.0.0.1 -n 1
+
+
+
+:end
+Call:LogMessage "========== Completed script %~0" "INFO"
+If NOT [%LogFile%]==[] echo Activity logged to %LogFile%
 
 :eof
